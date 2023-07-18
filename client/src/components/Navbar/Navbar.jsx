a07a5561f8ae18afa004bc98557f60e6c1ea0e16
ajinkya sontakke <ajinkyasontakke07@gmail.com>
2023-07-18 08:33:49 +0530

    commit1

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -12,7 +13,7 @@ import bars from "../../assets/bars-solid.svg";
 
 const Navbar = ({ handleSlideIn }) => {
   const dispatch = useDispatch();
-  var User = useSelector((state) => state.currentUserReducer);
+  const User = useSelector((state) => state.currentUserReducer);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -32,16 +33,52 @@ const Navbar = ({ handleSlideIn }) => {
     dispatch(setCurrentUser(JSON.parse(localStorage.getItem("Profile"))));
   }, [User?.token, dispatch]);
 
+  const renderAuthLinks = () => {
+    if (User === null) {
+      return (
+        <Link to="/Auth" className="nav-item nav-links">
+          Log in
+        </Link>
+      );
+    } else {
+      return (
+        <>
+          <Avatar
+            backgroundColor="#009dff"
+            px="10px"
+            py="7px"
+            borderRadius="50%"
+            color="white"
+          >
+            <Link
+              to={`/Users/${User?.result?._id}`}
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              {User.result.name.charAt(0).toUpperCase()}
+            </Link>
+          </Avatar>
+          <button className="nav-item nav-links" onClick={handleLogout}>
+            Log out
+          </button>
+        </>
+      );
+    }
+  };
+
   return (
     <nav className="main-nav">
       <div className="navbar">
-        <button className="slide-in-icon" onClick={() => handleSlideIn()}>
+        <button className="slide-in-icon" onClick={handleSlideIn}>
           <img src={bars} alt="bars" width="15" />
         </button>
         <div className="navbar-1">
           <Link to="/" className="nav-item nav-logo">
             <img src={logo} alt="logo" />
           </Link>
+          <form>
+            <input type="text" placeholder="Search..." />
+            <img src={search} alt="search" width="18" className="search-icon" />
+          </form>
           <Link to="/" className="nav-item nav-btn res-nav">
             About
           </Link>
@@ -51,38 +88,8 @@ const Navbar = ({ handleSlideIn }) => {
           <Link to="/" className="nav-item nav-btn res-nav">
             For Teams
           </Link>
-          <form>
-            <input type="text" placeholder="Search..." />
-            <img src={search} alt="search" width="18" className="search-icon" />
-          </form>
         </div>
-        <div className="navbar-2">
-          {User === null ? (
-            <Link to="/Auth" className="nav-item nav-links">
-              Log in
-            </Link>
-          ) : (
-            <>
-              <Avatar
-                backgroundColor="#009dff"
-                px="10px"
-                py="7px"
-                borderRadius="50%"
-                color="white"
-              >
-                <Link
-                  to={`/Users/${User?.result?._id}`}
-                  style={{ color: "white", textDecoration: "none" }}
-                >
-                  {User.result.name.charAt(0).toUpperCase()}
-                </Link>
-              </Avatar>
-              <button className="nav-item nav-links" onClick={handleLogout}>
-                Log out
-              </button>
-            </>
-          )}
-        </div>
+        <div className="navbar-2">{renderAuthLinks()}</div>
       </div>
     </nav>
   );
