f91db1b9d0a5270568afad78325b9f1c8ee48690
sainath-kadam <mydearsainathkadam@gmail.com>
2023-07-18 14:59:50 +0530

    Footer added

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+import "./Footer.css";
 
-export default function Footer() {
+const Footer = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_if1i5xw",
+        "template_nket8n8",
+        form.current,
+        "335ejCTAEklXOQOUw"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("Message sent successfully");
+          form.current.reset(); // Reset the form
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div>
-        contact
-      
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="tagline">
+        <span>Unlock Knowledge,</span>
+        <br />Empower Growth: <span>AskTask</span>, Learn, Evolve
+      </div>
+      <br />
+      <div className="credit">
+        By <span>Sainath Kadam</span> || rights reserved
+      </div>
+      <div className="contact-form">
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="user_name" placeholder="Your Name" required />
+          <input type="email" name="user_email" placeholder="Your Email" required />
+          <textarea name="message" placeholder="Your Message" required />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
