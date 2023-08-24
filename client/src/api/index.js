6b1aba457a8d9f7b716878707c5bcc17be6a6b40
sainath-kadam <mydearsainathkadam@gmail.com>
2023-08-24 22:53:16 +0530

    backend Deployed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://campusconnection.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
